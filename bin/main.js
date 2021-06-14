1ef7ddd46848199c2a1644080e7a6510447d4c90
hugoalh <32359235+hugoalh@users.noreply.github.com>
2021-06-14 16:49:35 +0800

    -

--- bin/main.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-/*==================
-[NodeJS] CLI Argument Parser (Bin)
-	Language:
-		NodeJS/10.13.0
-==================*/
-console.log(require("../lib/parse.js")(process.argv.slice(2)));
